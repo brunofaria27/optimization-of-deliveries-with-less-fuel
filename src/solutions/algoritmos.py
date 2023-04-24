20b8e79c4a694f0218c735a9166ec40c571382c6
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-04-24 20:16:32 -0300

    permutações - todas possibilidades possiveis de caminhos O(n!)

--- src/solutions/algoritmos.py
+++ src/solutions/algoritmos.py
@@ -5,9 +5,22 @@ import solutions.utils_solutions as utils # Funções que serão usadas tanto no
 
 RENDIMENTO_LITRO = 10
 
+def generate_permutations(lojas):
+    def helper(current_permutation, remaining_lojas):
+        if len(remaining_lojas) == 0:
+            return [current_permutation + [0]]
+        permutations = []
+        for i, loja_id in enumerate(remaining_lojas):
+            next_permutation = current_permutation + [loja_id]
+            next_remaining = remaining_lojas[:i] + remaining_lojas[i+1:]
+            permutations.extend(helper(next_permutation, next_remaining))
+        return permutations
+    loja_ids = list(lojas.keys())[1:]  # remove a loja 0
+    return helper([0], loja_ids)
+
 def bruteForce(filename, k_produtos):
-    print(utils.load_stores(filename))
-    print(int(k_produtos))
+    lojas = utils.load_stores(filename)
+    print(len(generate_permutations(lojas)))
 
 def branchAndBound(filename, k_produtos):
     print("Branch and bound")
