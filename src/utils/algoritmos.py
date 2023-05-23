daa8a77d26043e32a892f3118f9856b8a48926ec
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-05-23 20:59:53 -0300

    juntar a solução com a permutação

--- src/utils/algoritmos.py
+++ src/utils/algoritmos.py
@@ -12,26 +12,47 @@ import utils.utils_solutions as utils
 # Objetos
 from models.List import ListaLimitada
 
-def permutacoes(lojas):
+def permutacoes(lojas, k_produtos):
     lojas_filiais = list(lojas.keys())
     lojas_filiais.remove(0)  # Origem e destino não entram na permutação
 
-    def generate_permutations(lista_lojas):
-        if len(lista_lojas) == 0: return [[]]
+    melhor_caminho = None
+    melhor_custo = float('inf')
+    lista_melhor_custo = None
+    lista_itens_caminhao = None
 
-        permutacoes = []
+    def generate_permutations(lista_lojas, permutacao_atual):
+        nonlocal melhor_caminho, melhor_custo, lista_melhor_custo, lista_itens_caminhao # Atribuir valores a variáveis do escopo externo
+
+        if len(lista_lojas) == 0:
+            caminho = permutacao_atual + [0]
+            itens_caminhao, lojas_copy, lista_rendimento_plotar, lista_teste, caminho = calcula_viagem_total(lojas, caminho, int(k_produtos))
+            if itens_caminhao == 0 and verificaProdutosEntregues(lojas_copy):
+                custo_viagem = sum(lista_rendimento_plotar)
+                if custo_viagem < melhor_custo:
+                    melhor_caminho = caminho
+                    melhor_custo = custo_viagem
+                    lista_melhor_custo = lista_rendimento_plotar
+                    lista_itens_caminhao = lista_teste
+            return
+
         for i in range(len(lista_lojas)):
-            elementos_restantes = lista_lojas[:i] + lista_lojas[i + 1:] 
-            permutacoes_restantes = generate_permutations(elementos_restantes)
-            for perm in permutacoes_restantes:
-                permutacoes.append([lista_lojas[i]] + perm)
-        return permutacoes
-    permutacoes = generate_permutations(lojas_filiais)
-    permutacoes_com_zero = [[0] + perm + [0] for perm in permutacoes] # Adicionando destino e origem na permutação
-    return permutacoes_com_zero
+            loja_atual = lista_lojas[i]
+            elementos_restantes = lista_lojas[:i] + lista_lojas[i + 1:]
+            generate_permutations(elementos_restantes, permutacao_atual + [loja_atual])
+
+    generate_permutations(lojas_filiais, [0])
+    
+    return melhor_caminho, lista_melhor_custo, lista_itens_caminhao
 
 def calculate_distance(xA, yA, xB, yB):
     return math.sqrt((xA - xB)**2 + (yA - yB)**2)
+
+def verificaProdutosEntregues(lojas):
+    for entregas in lojas.values():
+        if entregas[2]:  # Verifica se a lista de entregas da loja não está vazia
+            return False
+    return True
 
 def calcula_viagem_total(lojas, caminho, k_produtos):
     lista_rendimento_plotar = list()
@@ -65,33 +86,13 @@ def calcula_viagem_total(lojas, caminho, k_produtos):
         distancia = calculate_distance(xA, yA, xB, yB)
         lista_rendimento_plotar.append(distancia / rendimento)
         lista_produtos.append(produtos_pegos.lista.copy())
-    return len(produtos_pegos), lojas_copy, lista_rendimento_plotar, lista_produtos
-
-def verificaProdutosEntregues(lojas):
-    for entregas in lojas.values():
-        if entregas[2]:  # Verifica se a lista de entregas da loja não está vazia
-            return False
-    return True
+    return len(produtos_pegos), lojas_copy, lista_rendimento_plotar, lista_produtos, caminho
 
 def bruteForce(filename, k_produtos):
     lojas = utils.load_stores(filename)
-    possiveis_caminhos = permutacoes(lojas)
-
-    melhor_caminho = None
-    melhor_custo = float('inf')
-    lista_melhor_custo = None
-    lista_itens_caminhao = None
-
-    for caminho in possiveis_caminhos:
-        itens_caminhao, lojas_copy, lista_rendimento_plotar, lista_teste = calcula_viagem_total(lojas, caminho, int(k_produtos))
-        custo_viagem = sum(lista_rendimento_plotar)
-        if itens_caminhao == 0 and custo_viagem < melhor_custo and verificaProdutosEntregues(lojas_copy):
-            melhor_caminho = caminho
-            melhor_custo = custo_viagem
-            lista_melhor_custo = lista_rendimento_plotar
-            lista_itens_caminhao = lista_teste
+    melhor_caminho, lista_melhor_custo, lista_itens_caminhao = permutacoes(lojas, int(k_produtos))
     print("Melhor caminho: " + str(melhor_caminho))
-    print("Custo total distância: " + str(melhor_custo))
+    print("Custo total distância: " + str(sum(lista_melhor_custo)))
     print("Itens caminhão: " + str(lista_itens_caminhao))
     plotBestTrip(lojas, melhor_caminho, lista_melhor_custo, lista_itens_caminhao)
 
