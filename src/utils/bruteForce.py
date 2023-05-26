dcb884316936b5026367da6e63875137dd70d465
Maria Luisa Raso <marialuisarasoo@gmail.com>
2023-05-26 10:17:50 -0300

    refinamento de variaveis na geracao de permutacoes brute force

--- src/utils/bruteForce.py
+++ src/utils/bruteForce.py
@@ -31,22 +31,22 @@ def permutacoes(lojas, k_produtos):
     melhor_caminho = None
     melhor_custo = float('inf')
     lista_melhor_custo = None
-    lista_itens_caminhao = None
+    lista_itens_do_caminhao = None
 
     def generate_permutations(lista_lojas, permutacao_atual):
-        nonlocal melhor_caminho, melhor_custo, lista_melhor_custo, lista_itens_caminhao # Atribuir valores a variáveis do escopo externo
+        nonlocal melhor_caminho, melhor_custo, lista_melhor_custo, lista_itens_do_caminhao # Atribuir valores a variáveis do escopo externo
 
         if len(lista_lojas) == 0:
             caminho = permutacao_atual + [0]
             PERMUTACOES.append(caminho)
-            itens_caminhao, lojas_copy, lista_rendimento_plotar, lista_teste, caminho = calculaViagemTotal(lojas, caminho, int(k_produtos))
-            if itens_caminhao == 0 and verificaProdutosEntregues(lojas_copy):
-                custo_viagem = sum(lista_rendimento_plotar)
-                if custo_viagem < melhor_custo:
+            itens_do_caminhao, lojas_copy, lista_rendimento_plotar, lista_itens_caminhao_teste_de_custo, caminho = calculaViagemTotal(lojas, caminho, int(k_produtos))
+            if itens_do_caminhao == 0 and verificaProdutosEntregues(lojas_copy):
+                custo_viagem_atual = sum(lista_rendimento_plotar)
+                if custo_viagem_atual < melhor_custo:
                     melhor_caminho = caminho
-                    melhor_custo = custo_viagem
+                    melhor_custo = custo_viagem_atual
                     lista_melhor_custo = lista_rendimento_plotar
-                    lista_itens_caminhao = lista_teste
+                    lista_itens_do_caminhao = lista_itens_caminhao_teste_de_custo
             return
 
         for i in range(len(lista_lojas)):
@@ -54,7 +54,7 @@ def permutacoes(lojas, k_produtos):
             elementos_restantes = lista_lojas[:i] + lista_lojas[i + 1:]
             generate_permutations(elementos_restantes, permutacao_atual + [loja_atual])
     generate_permutations(lojas_filiais, [0])
-    return melhor_caminho, lista_melhor_custo, lista_itens_caminhao
+    return melhor_caminho, lista_melhor_custo, lista_itens_do_caminhao
 
 def calculaViagemTotal(lojas, caminho, k_produtos):
     lista_rendimento_plotar = list()
@@ -94,11 +94,11 @@ def bruteForce(filename, k_produtos):
     PERMUTACOES.clear()
     time_start_bruteforce = time.time() # Inicio da execução brute force
     lojas, _ = fileTreatment.load_stores(filename)
-    melhor_caminho, lista_melhor_custo, lista_itens_caminhao = permutacoes(lojas, int(k_produtos))
+    melhor_caminho, lista_melhor_custo, lista_itens_do_caminhao = permutacoes(lojas, int(k_produtos))
     time_end_bruteforce = time.time() # Fim da execução brute force
     print("Melhor caminho: " + str(melhor_caminho))
     print("Custo total distância: " + str(sum(lista_melhor_custo)))
-    print("Itens caminhão: " + str(lista_itens_caminhao))
+    print("Itens caminhão: " + str(lista_itens_do_caminhao))
     print("Número de permutações BRUTE FORCE: " + str(len(PERMUTACOES)))
     print("Tempo de execução: " + str(time_end_bruteforce - time_start_bruteforce))
-    plotting.plotBestTrip(lojas, melhor_caminho, lista_melhor_custo, lista_itens_caminhao)
+    plotting.plotBestTrip(lojas, melhor_caminho, lista_melhor_custo, lista_itens_do_caminhao)
