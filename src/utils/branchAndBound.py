b6cc926157dd9dde3ccb0a41e029becf2ec542e8
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-05-30 18:06:58 -0300

    separação de listar produtos caminhao branch and bound

--- src/utils/branchAndBound.py
+++ src/utils/branchAndBound.py
@@ -74,13 +74,34 @@ def permutacoesBranchAndBound(lojas, entregas, k_produtos):
         for i in range(len(lista_lojas)):
             loja_atual = lista_lojas[i]
             elementos_restantes = lista_lojas[:i] + lista_lojas[i + 1:]
-            qtd_caminhao, lojas_copy, lista_rendimento_plotar, produtos_caminhao, caminho = calculaViagemTotalBranchAndBound(lojas, permutacao_atual + [0], int(k_produtos))
+            produtos_caminhao = pegaProdutosCaminhaoAtuais(lojas, permutacao_atual + [0], int(k_produtos))
             
             if podePassar(loja_atual, produtos_caminhao[-1], entregas):
                 generate_permutations(elementos_restantes, permutacao_atual + [loja_atual])
 
     generate_permutations(lojas_filiais, [0])
     return melhor_caminho, lista_melhor_custo, lista_itens_do_caminhao_total_caminho
+
+def pegaProdutosCaminhaoAtuais(lojas, caminho, k_produtos):
+    lista_de_produtos = list()
+    produtos_pegos = ListaLimitada(k_produtos)
+
+    for loja in range(len(caminho) - 1):
+        if caminho[loja] != 0:
+            produtos_loja = lojas[caminho[loja]][2].copy()
+
+            # Verificar se tem entrega
+            if caminho[loja] in produtos_pegos.lista:
+                for entrega in produtos_pegos.lista.copy():
+                    if entrega == caminho[loja]:
+                        produtos_pegos.remover_por_valor(caminho[loja])
+            
+            # Pegar produtos
+            if len(produtos_loja) != []:
+                for produto in produtos_loja:
+                    produtos_pegos.adicionar(produto) # Só adiciona se a lista não estiver cheia
+        lista_de_produtos.append(produtos_pegos.lista.copy())
+    return lista_de_produtos
 
 def calculaViagemTotalBranchAndBound(lojas, caminho, k_produtos):
     lista_rendimento_plotar = list()
