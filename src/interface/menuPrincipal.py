7a1067d7ea3af892249d1c3b0dc2e557ca609c2a
Bruno Rodrigues Faria <80405594+brunofaria27@users.noreply.github.com>
2023-06-04 22:11:27 -0300

    ALTERNATIVO 😎

--- src/interface/menuPrincipal.py
+++ src/interface/menuPrincipal.py
@@ -19,7 +19,7 @@ layout = [
     [[sg.Text("ESCOLHA ARQUIVO DE ENTRADA: ", font=("Helvetica", 15))]],
     [[sg.Text(font=("Helvetica", 15)), sg.FileBrowse("ESCOLHER", font=("Helvetica", 15), key="-FILEBROWSE-")]],
     [sg.Text("QTDE MÁXIMA DE PRODUTOS NO CAMINHÃO: ", font=("Helvetica", 15)), sg.Input(key="-K_PRODUTOS-", font=("Helvetica", 15), size=(10,1), enable_events=True, justification='center', text_color='black', background_color='white')],
-    [[sg.Text("ESCOLHA UM DOS ALGORÍTMOS: ", font=("Helvetica", 15)), sg.Button("FORÇA BRUTA", font=("Helvetica", 15)), sg.Button("BRANCH AND BOUND", font=("Helvetica", 15)), sg.Button("B&B TRIANGULACAO", font=("Helvetica", 15))]],
+    [[sg.Text("ESCOLHA UM DOS ALGORÍTMOS: ", font=("Helvetica", 15)), sg.Button("FORÇA BRUTA", font=("Helvetica", 15)), sg.Button("BRANCH AND BOUND", font=("Helvetica", 15)), sg.Button("B&B ALTERNATIVO", font=("Helvetica", 15))]],
 ]
 
 janela = sg.Window("TRABALHO 2 - PROJETO E ANÁLISE DE ALGORÍTMOS", layout, size=(1200, 300), text_justification="center", element_justification="center")
@@ -32,6 +32,6 @@ while True:
         BF.bruteForce(valores["-FILEBROWSE-"], valores["-K_PRODUTOS-"])
     if evento == "BRANCH AND BOUND":
         BB.branchAndBound(valores["-FILEBROWSE-"], valores["-K_PRODUTOS-"], False)
-    if evento == "B&B TRIANGULACAO":
+    if evento == "B&B ALTERNATIVO":
         BB.branchAndBound(valores["-FILEBROWSE-"], valores["-K_PRODUTOS-"], True)
 janela.close()
